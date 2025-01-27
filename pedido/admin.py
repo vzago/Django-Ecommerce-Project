735b25710d753fdf7e8c11ad18fcbf06462235e7
vzago <vitorzago94@gmail.com>
2025-01-27 15:14:17 -0300

    Creating models Order and OrderedItem

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class ItemPedidoInline(admin.TabularInline):
+    model = models.ItemPedido
+    extra = 1
+    
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'total', 'status']
+    search_fields = ['usuario__username', 'status']
+    list_filter = ['status']
+    list_editable = ['status']
+    inlines = [
+        ItemPedidoInline
+    ]
+    
+admin.site.register(models.Pedido, PedidoAdmin)
+admin.site.register(models.ItemPedido)
