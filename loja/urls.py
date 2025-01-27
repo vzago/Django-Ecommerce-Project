c29ee4982ca39bbed397b745fc59545ebaeb8212
vzago <vitorzago94@gmail.com>
2025-01-27 17:41:08 -0300

    Added URLs

--- loja/urls.py
+++ loja/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
     
     
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class ListaProdutos(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('ListarProduto')
+
+class DetalheProduto(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('DetalheProduto')
+    
+class AdicionarAoCarrinho(View):
+      def get(self,*args, **kwargs):
+        return HttpResponse('AdicionarAoCarrinho')
+
+class RemoverDoCarrinho(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('RemoverDoCarrinho')
+
+class Carrinho(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Carrinho')
+
+class Finalizar(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Finalizar')
